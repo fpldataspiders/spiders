febf2358f958ee6422e4382b4c2eaec35faa201f
fpldataspiders <30767037+fpldataspiders@users.noreply.github.com>
2017-12-01 11:11:03 +0100

    second spider for league table

--- spider.py
+++ spider.py
@@ -10,3 +10,20 @@ class TestSpider(scrapy.Spider):
         filename = response.url.split("/")[-1] + '.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
+            
+            
+class TestSpider(scrapy.Spider):
+    name = "leagueTABLEtop10K"
+    start_urls = ["https://fantasy.premierleague.com/a/leagues/standings/313/classic?phase=1&lsPage=" % 1]
+    def __init__(self):
+        self.page_number = 1
+
+    def start_requests(self):
+        # generate page IDs
+        for i in range (self.page_number,5, -1):
+            yield Request(url = URL % i, callback=self.parse)
+
+    def parse(self, response):
+        filename = response.url.split("/")[-1] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
